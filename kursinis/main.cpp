48666f2b6be4b1796aea974bcd97f507d9d5eb4f
Maximilian Los <noldor125@gmail.com>
2026-06-03 22:27:47 +0300

    kursinis v2

--- kursinis/main.cpp
+++ kursinis/main.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-// Bendrieji skaitikliai
+// ================= Skaitikliai =================
 long long comparisons = 0;
 long long swaps = 0;
 
-// Rusiavimas pagal atranką
+// ================= Selection Sort =================
 void selectionSort(vector<int>& arr) {
     int n = arr.size();
 
@@ -17,22 +17,23 @@ void selectionSort(vector<int>& arr) {
         int minIndex = i;
 
         for (int j = i + 1; j < n; j++) {
-            comparisons++; // atliksime palyginima
-            if (arr[j] < arr[minIndex])
+            comparisons++; // atliksime palyginimą
+            if (arr[j] < arr[minIndex]) {
                 minIndex = j;
+            }
         }
 
-        // elementu keitimas
+        // keičiam elementų vietas
         if (minIndex != i) {
             swap(arr[i], arr[minIndex]);
-            swaps++; // apskaiciuojame permaina
+            swaps++; // apskaičiuojame permainą
         }
     }
 }
 
-// Merge Sort
+// ================= Merge Sort =================
 
-// merge (sujungimas)
+// Dviejų masyvo dalių sujungimas
 void merge(vector<int>& arr, int left, int mid, int right) {
     vector<int> temp(right - left + 1);
 
@@ -42,15 +43,16 @@ void merge(vector<int>& arr, int left, int mid, int right) {
 
     while (i <= mid && j <= right) {
         comparisons++; // palyginimas
+
         if (arr[i] <= arr[j]) {
             temp[k++] = arr[i++];
         } else {
             temp[k++] = arr[j++];
         }
-        swaps++; // laikome tai judejimu
+
+        swaps++; // apskaičiuojame poslinkį
     }
 
-    // likusiu elementu kopijavimas
     while (i <= mid) {
         temp[k++] = arr[i++];
         swaps++;
@@ -61,14 +63,14 @@ void merge(vector<int>& arr, int left, int mid, int right) {
         swaps++;
     }
 
-    // graziname i pradini masyva
+    // kopijuojame atgal
     for (int t = 0; t < temp.size(); t++) {
         arr[left + t] = temp[t];
         swaps++;
     }
 }
 
-// rekursyvine funkcija
+// Rekursinis rūšiavimas
 void mergeSort(vector<int>& arr, int left, int right) {
     if (left < right) {
         int mid = (left + right) / 2;
@@ -80,27 +82,44 @@ void mergeSort(vector<int>& arr, int left, int right) {
     }
 }
 
-// Masyvo generavimas
-vector<int> generateArray(int size) {
-    vector<int> arr(size);
+// ================= Duomenų generavimas =================
 
+// Atsitiktinis masyvas
+vector<int> generateRandomArray(int size) {
+    vector<int> arr(size);
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(0, 10000);
+    uniform_int_distribution<> dis(0, 100000);
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
         arr[i] = dis(gen);
-    }
 
     return arr;
 }
 
-// Testavimas
-void test(int size) {
-    vector<int> arr1 = generateArray(size);
-    vector<int> arr2 = arr1;
+// Rūšiuotas masyvas
+vector<int> generateSortedArray(int size) {
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+        arr[i] = i;
+    return arr;
+}
 
-    // Selection Sort
+// Atvirkštine tvarka surūšiuotas masyvas
+vector<int> generateReverseArray(int size) {
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++)
+        arr[i] = size - i;
+    return arr;
+}
+
+// ================= Vieno masyvo testas =================
+void testWithArray(vector<int> arr) {
+
+    vector<int> arr1 = arr;
+    vector<int> arr2 = arr;
+
+    // ===== Selection Sort =====
     comparisons = 0;
     swaps = 0;
 
@@ -115,7 +134,7 @@ void test(int size) {
     cout << "Comparisons: " << comparisons << "\n";
     cout << "Swaps: " << swaps << "\n\n";
 
-    // Merge Sort
+    // ===== Merge Sort =====
     comparisons = 0;
     swaps = 0;
 
@@ -133,11 +152,32 @@ void test(int size) {
     cout << "=============================\n\n";
 }
 
-// main
+// ================= Išsamus testas =================
+void runTests(int size) {
+
+    cout << "=====================================\n";
+    cout << "Masyvo dydis: " << size << endl;
+    cout << "=====================================\n\n";
+
+    // 1. Atsitiktiniai duomenys
+    cout << "===== ATRANDAMIEJI DUOMENYS =====\n";
+    testWithArray(generateRandomArray(size));
+
+    // 2. Jau surūšiuoti
+    cout << "===== SURŪŠIUOTI DUOMENYS =====\n";
+    testWithArray(generateSortedArray(size));
+
+    // 3. Rūšiuoti atgaline tvarka
+    cout << "===== ATVIRKŠTINĖ TVARKA =====\n";
+    testWithArray(generateReverseArray(size));
+}
+
+// ================= main =================
 int main() {
-    test(5000);
-    test(10000);
-    test(50000);
+
+    runTests(5000);
+    runTests(10000);
+    runTests(50000);
 
     return 0;
 }
